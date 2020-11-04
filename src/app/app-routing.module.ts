31cb6e289cc3e0b16438328aa98bd6f813da4872
DlopezS98 <01dlopezs98@gmail.com>
2020-11-04 02:34:45 -0600

    adding protection to routes if user is not yet authenticated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,18 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './models/auth.guard';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'training', component: TrainingComponent },
+  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard] },
 ];
 
-@NgModule({ imports: [RouterModule.forRoot(routes)], exports: [RouterModule] })
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [AuthGuard],
+})
 export class AppRoutingModule {}
