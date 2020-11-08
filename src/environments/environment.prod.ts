e8c2069ec35afc92ab4aaf2f23df01b8fc95e5c5
DlopezS98 <01dlopezs98@gmail.com>
2020-11-07 22:07:41 -0600

    configure the project for deployment in production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB8qTCK3nS5hR41xRdgYlw00Q0Y24uVJO4",
+    authDomain: "ng-fitness-tracker-16243.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-16243.firebaseio.com",
+    projectId: "ng-fitness-tracker-16243",
+    storageBucket: "ng-fitness-tracker-16243.appspot.com",
+    messagingSenderId: "972630585367",
+    appId: "1:972630585367:web:58cb05376fd95f48a3ac52"
+  },
 };
